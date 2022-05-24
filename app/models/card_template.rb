18d6d62c3773fe680f38fffd52b0c7109511fe02
trungtq <trungtq@runsystem.net>
2022-05-24 08:44:20 +0700

    function update thanks card

--- app/models/card_template.rb
+++ app/models/card_template.rb
@@ -2,4 +2,9 @@ class CardTemplate < ApplicationRecord
   has_many :thank_cards
   has_one_attached :background
 
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :top, presence: true
+  validates :right, presence: true
+  validates :bottom, presence: true
+  validates :left, presence: true
 end
