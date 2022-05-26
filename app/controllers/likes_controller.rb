3e1167d9ce6138a873309c5b404cc42fe6b01fd7
trungtq <trungtq@runsystem.net>
2022-05-26 15:36:55 +0700

    function user like thanks card ( fix bug )

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,20 +1,30 @@
 class LikesController < ApplicationController
+  before_action :find_like, only: [:destroy]
+
   def create
     @like = current_user.likes.new(like_params)
     if !@like.save
       flash.now[:notice] = @like.errors.full_messages.to_sentence
-    end
+    else
+      flash.now[:success] = "Like Success!"
       redirect_to dashboard_path
+    end
   end
 
   def destroy
-    @like = current_user.likes.find(params[:id])
-    thank_card = @like.thank_card
-    @like.destroy
-    redirect_to dashboard_path
+    if @like.destroy
+      flash.now[:success] = "Unlike Success!"
+      redirect_to dashboard_path
+    else
+      flash.now[:notice] = @like.errors.full_messages.to_sentence
+    end
   end
 
   private
+  def find_like
+    @like = current_user.likes.find(params[:id])
+  end
+
   def like_params
     params.require(:like).permit(:thank_card_id)
   end
