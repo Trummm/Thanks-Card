cdc0e577862fa20299964dd7b9306be0658f7ea0
trungtq <trungtq@runsystem.net>
2022-07-04 14:02:20 +0700

    file change 6

--- db/migrate/20220519014202_create_card_templates.rb
+++ db/migrate/20220519014202_create_card_templates.rb
@@ -2,7 +2,6 @@ class CreateCardTemplates < ActiveRecord::Migration[6.1]
   def change
     create_table :card_templates do |t|
       t.string :name
-      t.string :image_url
       t.string :top
       t.string :right
       t.string :bottom
--- db/migrate/20220519032355_create_thank_cards.rb
+++ db/migrate/20220519032355_create_thank_cards.rb
@@ -3,8 +3,8 @@ class CreateThankCards < ActiveRecord::Migration[6.1]
     create_table :thank_cards do |t|
       t.references :user, null: false, foreign_key: true
       t.references :card_template, null: false, foreign_key: true
+      t.references :user_take_thank_card, null: false, foreign_key: true
       t.string :message
-      t.string :name
       t.boolean :approved, default: false
       
       t.timestamps
