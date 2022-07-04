6e5b4fbc00d7c03d18b866945803b35a91c2b255
trungtq <trungtq@runsystem.net>
2022-07-04 16:18:09 +0700

    file change 11

--- db/migrate/20220704073205_add_details_to_thank_cards.rb
+++
@@ -1,5 +0,0 @@
-class AddDetailsToThankCards < ActiveRecord::Migration[6.1]
-  def change
-    add_reference :thank_cards, :user_take_thank_card, null: false, foreign_key: true, if_not_exists: true
-  end
-end
