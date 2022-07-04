17c7236ecd0d91a2e732e845aa54cfb658f13eff
trungtq <trungtq@runsystem.net>
2022-07-04 14:14:57 +0700

    file change 7

--- db/migrate/20220519023038_remove_image_from_card_template.rb
+++ db/migrate/20220519023038_remove_image_from_card_template.rb
@@ -1,5 +1,5 @@
 class RemoveImageFromCardTemplate < ActiveRecord::Migration[6.1]
   def change
-    remove_column :card_templates, :image_url, :string
+    remove_column :card_templates, :image_url, :string, if_exists: true
   end
 end
