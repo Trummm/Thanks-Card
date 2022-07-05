cb058daf882199cdeab2b0ae458b262c71cbaf4b
trungtq <trungtq@runsystem.net>
2022-07-05 08:56:16 +0700

    file change 12

--- db/migrate/20220519032355_create_thank_cards.rb
+++ db/migrate/20220519032355_create_thank_cards.rb
@@ -3,11 +3,12 @@ class CreateThankCards < ActiveRecord::Migration[6.1]
     create_table :thank_cards do |t|
       t.references :user, null: false, foreign_key: true
       t.references :card_template, null: false, foreign_key: true
-      t.references :user_take_thank_card, null: false, foreign_key: true, if_exists: true
+      t.references :user_take_thank_card, index: true
       t.string :message
       t.boolean :approved, default: false
       
       t.timestamps
     end
+    add_foreign_key :thank_cards, :users, column: :user_take_thank_card_id
   end
 end
