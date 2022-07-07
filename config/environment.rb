60db8ba140141b344d5a718b3fa3f1c78bb56fb5
trungtq <trungtq@runsystem.net>
2022-07-08 00:26:50 +0700

    file change 18

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
-ActionMailer::Base.default_content_type = "text/html"
-
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            '<username>',
-  password:             '<password>',
-  authentication:       'plain',
-  enable_starttls_auto: true  
-}
+
